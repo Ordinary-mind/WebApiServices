1b2dee21524a409b54803bf5a20bca91f14b36a7
ordinaryMind <1633263519@qq.com>
2020-10-19 20:28:42 +0800

    初始化启动配置

---
+++ WebApiServices/ConfigConsts.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiServices
+{
+    public class ConfigConsts
+    {
+        public static string StartHost { get; set; }
+    }
+}
---
+++ WebApiServices/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace WebApiServices
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            SetConfigConsts();
+            BuildWebHost(args).Run();
+            
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .UseUrls(ConfigConsts.StartHost)
+                .UseStartup<Startup>()
+                .Build();
+
+        public static void SetConfigConsts()
+        {
+            var config = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("conf.json").Build();
+            ConfigConsts.StartHost = config["startHost"];
+        }
+    }
+}
